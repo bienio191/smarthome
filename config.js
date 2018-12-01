774de701504194952c224e2193572faf680ee64d
lukasz <lukasz.bieniewicz@gmail.com>
2018-12-01 21:21:24 +0100

    fixes

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    hue_user_id: 'EZNBevLPlA12Y7g9-sXTzvi8qPV2M02PJqvfxG5O',
+    hue_host: 'http://192.168.1.251',
+    home_longitude: '20.701697',
+    home_latitude: '52.447591',
+    sleep_hour: '22',
+    pig_bulb_id: '1',
+    pig_bulb_brightness: '85'
+}
+  
+module.exports = config;
